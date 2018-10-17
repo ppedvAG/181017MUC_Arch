2cbed69f6d1ebeff22f39ebd92e076888b912bcc
Michael Zöhling <michael.zoehling@outlook.com>
2018-10-17 15:34:09 +0200

    Composite - Simpel

---
+++ GoF/HalloComposite/Aufgabe.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloComposite
+{
+    abstract class Aufgabe
+    {
+        public string Beschreibung { get; set; }
+        public bool IstErledigt { get; set; }
+        public abstract void AufgabeAbarbeiten();
+    }
+
+    class Einzelaufgabe : Aufgabe
+    {
+        public override void AufgabeAbarbeiten()
+        {
+            Console.WriteLine($"{Beschreibung} --- Einzelaufgabe wird abgearbeitet ...");
+        }
+    }
+
+    class AufgabeListe : Aufgabe
+    {
+        public List<Aufgabe> Aufgabenliste { get; set; } = new List<Aufgabe>();
+        public void AufgabeHinzufügen(Aufgabe aufgabe)
+        {
+            // Nach validierung -> hinzufügen
+            Aufgabenliste.Add(aufgabe);
+        }
+
+        public override void AufgabeAbarbeiten()
+        {
+            Console.WriteLine($"AufgabenListe {Beschreibung} startet das Abarbeiten");
+            foreach (var item in Aufgabenliste)
+            {
+                item.AufgabeAbarbeiten();
+            }
+            Console.WriteLine($"AufgabenListe {Beschreibung} beendet das Abarbeiten");
+        }
+    }
+}
--- GoF/HalloComposite/Program.cs
+++ GoF/HalloComposite/Program.cs
@@ -10,6 +10,26 @@ namespace HalloComposite
     {
         static void Main(string[] args)
         {
+            AufgabeListe schulung = new AufgabeListe();
+            schulung.Beschreibung = ".NET Architektur-Kurs";
+            AufgabeListe vormittag = new AufgabeListe();
+            vormittag.Beschreibung = "Framework und Patterns";
+            vormittag.AufgabeHinzufügen(new Einzelaufgabe { Beschreibung = ".NET Framework" });
+            vormittag.AufgabeHinzufügen(new Einzelaufgabe { Beschreibung = " Singleton" });
+
+            AufgabeListe nachmittag = new AufgabeListe();
+            nachmittag.Beschreibung = "Patterns";
+            nachmittag.AufgabeHinzufügen(new Einzelaufgabe { Beschreibung = "Builder" });
+            nachmittag.AufgabeHinzufügen(new Einzelaufgabe { Beschreibung = "FactoryMethode" });
+
+            schulung.AufgabeHinzufügen(vormittag);
+            schulung.AufgabeHinzufügen(nachmittag);
+
+            schulung.AufgabeAbarbeiten();
+
+
+            Console.WriteLine("---ENDE---");
+            Console.ReadKey();
         }
     }
 }
