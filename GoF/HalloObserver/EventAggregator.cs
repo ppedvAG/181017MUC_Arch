10674969fed790d10d0ff76a3674e3c293dbf431
Michael Zöhling <michael.zoehling@outlook.com>
2018-10-17 17:14:07 +0200

    Observer

---
+++ GoF/HalloObserver/EventAggregator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HalloObserver
+{
+    static class EventAggregator
+    {
+        // Heizung: Subscribe an alle Nachrichten "Messwert" vom Datentyp "Sensor"
+        // -> Liste von allen subscribern, 
+
+        // Subscribe: An was ?, Was soll ausgeführt werden (Callback)
+        private static List<Subscriber> subscribers = new List<Subscriber>();
+
+        public static void Subscribe(object subscriberInstance,string message,Action<object> callback)
+        {
+            subscribers.Add(new Subscriber { SubscriberInstance = subscriberInstance, Message = message, Callback = callback });
+        }
+
+        public static void Unsubscribe(object subscriberInstance,string message)
+        {
+            subscribers.RemoveAll(x => x.SubscriberInstance == subscriberInstance && x.Message == message);
+        }
+
+        public static void Publish(string message,object value)
+        {
+            foreach (var item in subscribers.Where(x => x.Message == message))
+            {
+                item.Callback?.Invoke(value);
+            }
+        }
+    }
+
+    class Subscriber
+    {
+        public object SubscriberInstance { get; set; }
+        public string Message { get; set; }
+        public Action<object> Callback { get; set; }
+    }
+}
--- GoF/HalloObserver/Program.cs
+++ GoF/HalloObserver/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,9 +11,43 @@ namespace HalloObserver
     {
         static void Main(string[] args)
         {
+            Sensor aussensensor = new Sensor();
+            Heizung wohnzimmerheizung = new Heizung();
+
+            EventAggregator.Subscribe(wohnzimmerheizung, "Messwert", wohnzimmerheizung.MesswertVerarbeiten);
+
+            aussensensor.MesswertSchicken();
+            aussensensor.MesswertSchicken();
+            EventAggregator.Unsubscribe(wohnzimmerheizung, "Messwert");
+
+            aussensensor.MesswertSchicken();
+
 
             Console.WriteLine("---ENDE---");
             Console.ReadKey();
+        }
+    }
+
+    class Heizung
+    {
+        public void MesswertVerarbeiten(object wert)
+        {
+            // object -> immer ein int
+            int temp = (int)wert;
+            if(temp < 10)
+                Console.WriteLine($"Heizung wird aufgedreht: Temp {temp}");
+            else if(temp >35)
+                Console.WriteLine($"Klimaanlage wird aufgedreht: Temp {temp}");
+            else
+                Console.WriteLine($"Gemütlich warm : Temp {temp}");
+        }
+    }
+
+    class Sensor
+    {
+        public void MesswertSchicken()
+        {
+            EventAggregator.Publish("Messwert", new Random().Next(0, 100));
         }
     }
 }
