9678d18b57ffe2cf3e3b121f5db3584ea65ef1d7
Michael Zöhling <michael.zoehling@outlook.com>
2018-10-18 12:01:40 +0200

    Grundlegende Unit-Tests

---
+++ GoldCricket/ppedv.GoldCricket.Data.EF.Tests/EFContextTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace ppedv.GoldCricket.Data.EF.Tests
+{
+    [TestFixture] // mstest: TestClass
+    public class EFContextTests
+    {
+
+        //public void klassenname_methodenname_ausgangsituation_erwartetesErgebnis
+        [Test] // mstest: TestMethod, Xunit: Fact
+        public void EFContext_Can_Create_Instance()
+        {
+            // AAA - Prinzip
+
+            // Arrange -> Initialisieren
+            // Act     -> Ausführen
+            // Assert  -> Ergebnis interpretieren
+
+            EFContext ctx = new EFContext();
+            Assert.IsNotNull(ctx);
+        }
+
+        public void EFContext_Can_Create_DB()
+        {
+
+        }
+
+    }
+}
---
+++ ppedv.GoldCricket.Data.EF.TestsNUnit/Class1.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.GoldCricket.Data.EF.TestsNUnit
+{
+    [TestFixture] // mstest: TestClass
+    public class EFContextTests
+    {
+
+        //public void klassenname_methodenname_ausgangsituation_erwartetesErgebnis
+        [Test] // mstest: TestMethod, Xunit: Fact
+        public void EFContext_Can_Create_Instance()
+        {
+            // AAA - Prinzip
+
+            // Arrange -> Initialisieren
+            // Act     -> Ausführen
+            // Assert  -> Ergebnis interpretieren
+
+            EFContext ctx = new EFContext();
+            Assert.IsNotNull(ctx);
+        }
+
+        [Test]
+        public void EFContext_Can_Create_DB()
+        {
+            EFContext ctx = new EFContext();
+
+            if (ctx.Database.Exists())
+                ctx.Database.Delete();
+
+            Assert.IsFalse(ctx.Database.Exists()); // Clean-Zustand: keine DB
+
+            ctx.Database.Create();
+            Assert.IsTrue(ctx.Database.Exists()); // DB muss jetzt da sein !
+        }
+
+    }
+}
