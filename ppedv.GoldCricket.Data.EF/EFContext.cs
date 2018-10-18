7ad1b1a5476ba8ca53f47a66dbd219c05780a6fd
Michael Zöhling <michael.zoehling@outlook.com>
2018-10-18 11:15:02 +0200

    EF - Basisversion

---
+++ ppedv.GoldCricket.Data.EF/EFContext.cs
@@ -0,0 +1,16 @@
+﻿using ppedv.GoldCricket.Domain;
+using System;
+using System.Data.Entity;
+
+namespace ppedv.GoldCricket.Data.EF
+{
+    public class EFContext : DbContext
+    {
+        //public EFContext() : base("Server=.;Database=GoldCricket;Trusted_Connection=true") { }
+        public EFContext() : base("Server=(localdb)\\mssqllocaldb;Database=GoldCricket;Trusted_Connection=true;AttachDbFilename=C:\\temp\\meineDB.mdf") { }
+
+        public DbSet<Application> Application { get; set; }
+        public DbSet<Person> Person { get; set; }
+        public DbSet<Domain.Database> AppDatabase { get; set; }
+    }
+}
