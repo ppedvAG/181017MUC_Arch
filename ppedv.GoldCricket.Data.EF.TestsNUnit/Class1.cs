a007761c66a84ef9e57d1f375e966f5cd0f18ab1
Michael Zöhling <michael.zoehling@outlook.com>
2018-10-18 12:26:39 +0200

    CRUD Person

--- ppedv.GoldCricket.Data.EF.TestsNUnit/Class1.cs
+++ ppedv.GoldCricket.Data.EF.TestsNUnit/Class1.cs
@@ -1,4 +1,5 @@
 ﻿using NUnit.Framework;
+using ppedv.GoldCricket.Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -39,5 +40,46 @@ namespace ppedv.GoldCricket.Data.EF.TestsNUnit
             Assert.IsTrue(ctx.Database.Exists()); // DB muss jetzt da sein !
         }
 
+        [Test]
+        public void EFContext_Can_CRUD_Person() // Create, Read, Update, Delete
+        {
+            Person p = new Person { Vorname = "Franz", Nachname = "Ose", Alter = 30, Kontostand = 2000m };
+            string neuerNachname = "Osinger";
+
+            // Create
+            using (EFContext ctx = new EFContext())
+            {
+                ctx.Person.Add(p); // Insert in Person-Tabelle
+                ctx.SaveChanges(); // Ab hier hat p auch eine ID !
+            }
+
+            using (EFContext ctx = new EFContext())
+            {
+                var geladenePerson = ctx.Person.Find(p.ID);
+                Assert.IsNotNull(geladenePerson);
+                Assert.AreEqual(p.Nachname, geladenePerson.Nachname); // Read
+
+                //Update
+                geladenePerson.Nachname = neuerNachname;
+                ctx.SaveChanges(); // UPDATE -> Person hat sich verändert -> Änderung in DB nachtragen
+            }
+
+            using (EFContext ctx = new EFContext())
+            {
+                var geladenePerson = ctx.Person.Find(p.ID); // neu laden, da wir die Person nicht aus dem Cache haben wollen !!!
+                Assert.IsNotNull(geladenePerson);
+                Assert.AreEqual(neuerNachname, geladenePerson.Nachname); // Update-Test
+
+                //Delete
+                ctx.Person.Remove(geladenePerson);
+                ctx.SaveChanges();
+            }
+
+            using (EFContext ctx = new EFContext())
+            {
+                var geladenePerson = ctx.Person.Find(p.ID); // Person existiert nicht -> NULL
+                Assert.IsNull(geladenePerson);
+            }
+        }
     }
 }
--- ppedv.GoldCricket.Data.EF/EFContext.cs
+++ ppedv.GoldCricket.Data.EF/EFContext.cs
@@ -1,6 +1,7 @@
 ﻿using ppedv.GoldCricket.Domain;
 using System;
 using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
 
 namespace ppedv.GoldCricket.Data.EF
 {
@@ -12,5 +13,16 @@ namespace ppedv.GoldCricket.Data.EF
         public DbSet<Application> Application { get; set; }
         public DbSet<Person> Person { get; set; }
         public DbSet<Domain.Database> AppDatabase { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            // Konfiguration für das EF
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+
+            modelBuilder.Entity<Person>().Property(x => x.Kontostand)
+                                         .HasColumnName("Ka_ching")
+                                         .IsRequired();
+        }
+
     }
 }
